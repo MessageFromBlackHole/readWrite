9cd0e66b12778d432f27c253de6ec0ed8b753f5f
Loowi <loowi@moon.home>
2020-04-13 01:46:19 +0200

    vision added

---
+++ see.py
@@ -0,0 +1,46 @@
+# from cmd: python see.py faces two_persons.jfif
+import argparse
+from google.cloud import vision
+import io
+
+# [START vision_face_detection]
+def detect_faces(path):
+    """Detects faces in an image."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+
+    for face in faces:
+        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in face.bounding_poly.vertices])
+
+        print('face bounds: {}'.format(','.join(vertices)))
+
+def run_local(args):
+    detect_faces(args.path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(dest='command')
+
+    detect_faces_parser = subparsers.add_parser(
+        'faces', help=detect_faces.__doc__)
+    detect_faces_parser.add_argument('path')
+    args = parser.parse_args()
+
+    run_local(args)
